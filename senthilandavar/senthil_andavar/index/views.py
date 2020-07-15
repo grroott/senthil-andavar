f9a22dc59bb59e107147cebbba617384454b9b5a
Gokul nath <gokulnath634@gmail.com>
2020-07-15 13:15:02 +0530

    Implemented forms for contact info

--- senthilandavar/senthil_andavar/index/views.py
+++ senthilandavar/senthil_andavar/index/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from .models import Contact
+from .forms import ContactForm
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-	return render(request, 'index/home.html')
+
+	if request.method == "POST":
+		form = ContactForm(request.POST)
+		if form.is_valid():
+			form.save()
+			subject = form.cleaned_data.get('subject')
+			messages.success(request, f'   We have received your contact information. We will get back to you as soon as possible! Meanwhile, you can always reach us on +91 94866 47406 / +91 98427 47406')
+			return redirect('home-page')
+	else:	
+		form = ContactForm()
+
+	return render(request, 'index/home.html', {'form':form})
+
+
