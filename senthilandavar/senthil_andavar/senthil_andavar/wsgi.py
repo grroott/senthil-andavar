b79cbf3db429a43b8b8785546f2e6e8d885380be
Gokul nath <gokulnath634@gmail.com>
2020-07-15 10:42:02 +0530

    Migrated the existing files to django

---
+++ senthilandavar/senthil_andavar/senthil_andavar/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for senthil_andavar project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'senthil_andavar.settings')
+
+application = get_wsgi_application()
