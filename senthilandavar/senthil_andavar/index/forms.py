73d091de0d8f3479d70ae8c8fcf313a7ef291bb8
Gokul nath <gokulnath634@gmail.com>
2020-07-16 19:58:02 +0530

    Adjusted django forms to use existing design

--- senthilandavar/senthil_andavar/index/forms.py
+++ senthilandavar/senthil_andavar/index/forms.py
@@ -6,3 +6,10 @@ class ContactForm(forms.ModelForm):
 	class Meta:
 		model = Contact
 		fields = ['name', 'email', 'subject', 'message']
+
+		widgets = {
+		'name' : forms.TextInput(attrs={'class': 'input form-control', 'placeholder': 'Your Name'}),
+		'email' : forms.TextInput(attrs={'class': 'input form-control', 'placeholder': 'Your Email'}),
+		'subject' : forms.TextInput(attrs={'class': 'input form-control', 'placeholder': 'Subject'}),
+		'message' : forms.Textarea(attrs={'class': 'textarea form-control', 'rows': 4, 'placeholder': 'Message'})
+		}
