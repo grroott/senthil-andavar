002951acf1399e0016e880b1d6699ba3a2195a75
Gokul nath <gokulnath634@gmail.com>
2020-07-16 23:37:15 +0530

    Environ vars

--- senthil_andavar/settings.py
+++ senthil_andavar/settings.py
@@ -20,12 +20,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4pj=y###3efrvn0)vmjdrdvbczk2q-n$_amxh3mh2ib@222naz'
+# SECRET_KEY = '4pj=y###3efrvn0)vmjdrdvbczk2q-n$_amxh3mh2ib@222naz'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['senthilandavar.herokuapp.com']
 
 
 # Application definition
