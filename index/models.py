8ca6b43a98cad3cf174a7872ac0483a77b308586
Gokul nath <gokulnath634@gmail.com>
2020-07-17 00:05:26 +0530

    Adjusting field for mobile

--- index/models.py
+++ index/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Contact(models.Model):
     name = models.CharField(max_length=100)
-    mobile = models.CharField(max_length=20, null=True)
+    mobile = models.CharField(max_length=20)
     subject = models.CharField(max_length=300)
     message = models.TextField()
 
