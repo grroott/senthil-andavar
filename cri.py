80bb3b206fb8a2301919342211cda8cb81088f6e
Gokul nath <gokulnath634@gmail.com>
2020-10-13 10:05:20 +0530

    Fixed Twitter proxy issue

--- cri.py
+++ cri.py
@@ -1,14 +1,1 @@
-
-import time
-from pycricbuzz import Cricbuzz
-
-c = Cricbuzz()
-matches = c.matches()
-
-print(matches)
-
-for i in matches:
-    if  i['srs'] == 'Indian Premier League 2020' and i['mchstate'] == 'inprogress' :
-        print(i)
-        mid = i['id']
-        print(mid)
+print("Hello")
